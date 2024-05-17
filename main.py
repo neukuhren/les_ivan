1693beb73c10d867b6a778a894c3cb37c5975b0e
exclusive.miha <exclusive.miha@gmail.com>
2024-05-17 18:07:48 +0300

    added file .env

--- main.py
+++ main.py
@@ -1,24 +1,33 @@
-# pip install requests
-# pip install beautifulsoup4
+import telebot
 import requests
 from bs4 import BeautifulSoup
-import telebot
 from telebot.types import ReplyKeyboardMarkup, KeyboardButton
 import time
 import hashlib
 # pip install lxml
 
+import os
+# pip install python-dotenv
+from dotenv import load_dotenv  # Для безопасного хранения переменных окружения
+load_dotenv()  # Теперь переменные окружения из файла .env доступны
 
-bot = telebot.TeleBot(TOKEN)
+
+TOKEN_TG_BOT = os.getenv('TOKEN_TG_BOT')
+"""Токен для работы с API Telegram"""
+bot = telebot.TeleBot(TOKEN_TG_BOT)
 
 
+ANECDOTICA_API_PID = os.getenv('ANECDOTICA_API_PID')
+"""Имя профиля anecdotica"""
+ANECDOTICA_API_TOKEN = os.getenv('ANECDOTICA_API_TOKEN')
+"""Cекретный токен для работы с API сайта anecdotica.ru"""
+ANECDOTICA_API_KEY = os.getenv('ANECDOTICA_API_KEY')
+"""Cекретный ключ для работы с API сайта anecdotica.ru"""
 
-
-# формируем строку параметров
-query = 'pid=' + PID + '&method=getRandItem&uts=' + str(int(time.time()))
-
-signature = hashlib.md5((query + KEY).encode())  # получаем цифровую подпись
+query = 'pid=' + ANECDOTICA_API_PID + '&method=getRandItem&uts'
+signature = hashlib.md5((query + ANECDOTICA_API_KEY).encode())  # получаем цифровую подпись
 url = 'http://anecdotica.ru/api?' + query + '&hash=' + signature.hexdigest()
+
 
 
 def parse_anekdot() -> str :
@@ -29,7 +38,6 @@ def parse_anekdot() -> str :
     soup = BeautifulSoup(response.text, 'xml')
     anekdot = soup.find('item').text
     return anekdot
-    
 
 
 # Обработчик сообщений
@@ -40,25 +48,21 @@ def handler_messages(message):
         # Набор кнопок
         markup = ReplyKeyboardMarkup(row_width=1)
         btn1 = KeyboardButton('Пришли анекдот')
-        btn2 = KeyboardButton('Хватит')
+        btn2 = KeyboardButton('Хватит с меня шуточек..')
         markup.add(btn1, btn2)
         bot.send_message(chat_id=message.chat.id,
-                         text='Здравствуйте! Для получения анекдота нажмите кнопку',
-                         reply_markup = markup
-                         )
-    
+                         text='Здраствуйте, для анекдотика нажми кнопочку)',
+                         reply_markup= markup)
     elif message.text == 'Пришли анекдот':
         text_anekdot = parse_anekdot()
         if text_anekdot:
             bot.send_message(chat_id=message.chat.id, text=text_anekdot)
         else:
-            print('ОШИБКА! АНЕДКОТ НЕ ПОЛУЧЕН!')
-    
-    elif message.text == 'Хватит':
-        bot.send_message(chat_id=message.chat.id, text='Ок! Приходите еще!')
-
-
-bot.polling()
+            print('ошибочка, шутки нету(')
+    elif message.text == 'Хватит с меня шуточек..':
+        bot.send_message(chat_id=message.chat.id, text='Ладно, приходите ещё!')
 
 
 
+
+bot.polling(non_stop=True, interval=5)
